bf4c6b12a55d20f42e3461df069753ae9ea66203
kiokogit <kiokovincent12@gmail.com>
2023-04-16 22:48:26 +0300

    db push

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: gunicorn app.wsgi
+release: python manage.py migrate
--- app/settings/production.py
+++ app/settings/production.py
@@ -7,7 +7,16 @@ load_dotenv()
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-g$1n#9%#q1(_a_!m_#e&j=js)zf-=e5a8m+(fa#l=z*9ro*-s4'
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+DATABASES = {
+    'default':{
+        'ENGINE':'django.db.backends.sqlite3',
+         'NAME': BASE_DIR / 'db_prod.sqlite3'
+        
+    }
+}
+
 
 # DATABASES = {
 #         'default': {
@@ -60,27 +69,27 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 #     }
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DBNAME'),
-        'USER': os.environ.get('DBUSER'),
-        'PASSWORD': os.environ.get('DBPASS'),
-        'CONN_MAX_AGE': 1000,
-        'OPTIONS': {
-            'options': '-c search_path={}'.format(os.environ.get('DBSCHEMA'))
-        },
-        'HOST': 'db',
-        'PORT': int(os.environ.get('DBPORT')), #type: ignore
-        'TEST': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.environ.get('DBNAME'),
-            'USER': os.environ.get('DBUSER'),
-            'PASSWORD': os.environ.get('DBPASS'),
-            'OPTIONS': {
-                'options': '-c search_path={}'.format(os.environ.get('DBSCHEMA'))
-            },
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.environ.get('DBNAME'),
+#         'USER': os.environ.get('DBUSER'),
+#         'PASSWORD': os.environ.get('DBPASS'),
+#         'CONN_MAX_AGE': 1000,
+#         'OPTIONS': {
+#             'options': '-c search_path={}'.format(os.environ.get('DBSCHEMA'))
+#         },
+#         'HOST': 'db',
+#         'PORT': int(os.environ.get('DBPORT')), #type: ignore
+#         'TEST': {
+#             'ENGINE': 'django.db.backends.postgresql',
+#             'NAME': os.environ.get('DBNAME'),
+#             'USER': os.environ.get('DBUSER'),
+#             'PASSWORD': os.environ.get('DBPASS'),
+#             'OPTIONS': {
+#                 'options': '-c search_path={}'.format(os.environ.get('DBSCHEMA'))
+#             },
 
-        },
-    }
-}
+#         },
+#     }
+# }
